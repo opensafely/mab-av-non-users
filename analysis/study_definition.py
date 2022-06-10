f0dd164653a61be1bcad6b2718996e7b8cb42792
johntaz <john.tazare1@lshtm.ac.uk>
2022-06-10 18:20:05 +0100

    Update study_definition.py

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -50,25 +50,47 @@ study = StudyDefinition(
     # Define the study population
     population = patients.satisfying(
         """
-        covid_test_positive 
+        covid_test_positive AND NOT covid_positive_previous_90_days
 
         """,
         ),
 
     # INCLUSION CRITERIA 
-        covid_test_positive = patients.with_test_result_in_sgss(
-            pathogen = "SARS-CoV-2",
-            test_result = "positive",
-            between = ["study_start", "study_end"],
-            returning = "date",
-            date_format = "YYYY-MM-DD",
-            find_first_match_in_period = True,
-            restrict_to_earliest_specimen_date = False,
-            return_expectations = {
-                "date": {"earliest": "study_start", "latest" : "study_end"},
-                "rate": "exponential_increase",
-                "incidence": 0.6
-            },
-        ),
-    
+    covid_test_positive = patients.with_test_result_in_sgss(
+        pathogen = "SARS-CoV-2",
+        test_result = "positive",
+        returning = "binary_flag",
+        on_or_after = "study_start",
+        find_first_match_in_period = True,
+        restrict_to_earliest_specimen_date = False,
+        return_expectations = {
+        "incidence": 0.8
+        },
+     ),
+    covid_test_positive_date = patients.with_test_result_in_sgss(
+        pathogen = "SARS-CoV-2",
+        test_result = "positive",
+        find_first_match_in_period = True,
+        restrict_to_earliest_specimen_date = False,
+        returning = "date",
+        date_format = "YYYY-MM-DD",
+        on_or_after = "study_start",
+        return_expectations = {
+            "date": {"earliest": "study_start"},
+            "incidence": 0.9
+        },
+    ),
+
+  ### Positive covid test last 90 days 
+    covid_positive_previous_90_days = patients.with_test_result_in_sgss(
+        pathogen = "SARS-CoV-2",
+        test_result = "positive",
+        returning = "binary_flag",
+        between = ["covid_test_positive_date - 91 days", "covid_test_positive_date - 1 day"],
+        find_last_match_in_period = True,
+        restrict_to_earliest_specimen_date = False,
+        return_expectations = {
+            "incidence": 0.03
+        },
+    ),
 )
