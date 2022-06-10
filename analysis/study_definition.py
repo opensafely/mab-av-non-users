47196f1176429ab32329b412feb51c46d300f8cb
johntaz <john.tazare1@lshtm.ac.uk>
2022-06-10 18:36:04 +0100

    update

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,53 +1,45 @@
-######################################
-
-# This script provides the formal specification of the study data that will
-# be extracted from the OpenSAFELY database.
-
-######################################
-
 # IMPORT STATEMENTS ----
-# Import code building blocks from cohort extractor package
 
+## Import code building blocks from cohort extractor package
 from cohortextractor import (
-    StudyDefinition,
-    patients,
-    filter_codes_by_category,
-    combine_codelists,
-    codelist_from_csv,
-    codelist,
-    Measure
+  StudyDefinition,
+  patients,
+  codelist_from_csv,
+  codelist,
+  filter_codes_by_category,
+  combine_codelists,
+  Measure
 )
 
-# Import codelists from codelist.py (which pulls them from the codelist folder)
+## Import codelists from codelist.py (which pulls them from the codelist folder)
 from codelists import *
-
-# Import config variables (dates, list of demographics and list of
-# comorbidities)
-
-# Import json module
-import json
-with open("./lib/design/study-dates.json") as f:
-  study_dates = json.load(f)
-
-start_date = study_dates["start_date"]
-end_date = study_dates["end_date"]
-
+  
+  
 # DEFINE STUDY POPULATION ----
-# Define study population and variables
-study = StudyDefinition(
 
-    # Configure the expectations framework
-    default_expectations = {
-        "date": {"earliest": "2021-11-01", "latest": "today"},
-        "rate": "uniform",
-        "incidence": 0.05,
-    },
-    
-    # Set index date to start date
-    study_start=start_date,
-    study_end=end_date,
-    
-    # Define the study population
+## Define study time variables
+from datetime import date
+
+campaign_start = "2021-12-16"
+end_date = "2022-02-10"
+
+
+## Define study population and variables
+study = StudyDefinition(
+  
+  # PRELIMINARIES ----
+  
+  ## Configure the expectations framework
+  default_expectations = {
+    "date": {"earliest": "2021-12-16", "latest": end_date},
+    "rate": "uniform",
+    "incidence": 0.05,
+  },
+  
+  ## Define index date
+  studystart_date = campaign_start,
+  
+  # POPULATION ----
     population = patients.satisfying(
         """
         covid_test_positive AND NOT covid_positive_previous_90_days
@@ -60,7 +52,7 @@ study = StudyDefinition(
         pathogen = "SARS-CoV-2",
         test_result = "positive",
         returning = "binary_flag",
-        on_or_after = "study_start",
+        on_or_after = "studystart_date",
         find_first_match_in_period = True,
         restrict_to_earliest_specimen_date = False,
         return_expectations = {
@@ -74,9 +66,9 @@ study = StudyDefinition(
         restrict_to_earliest_specimen_date = False,
         returning = "date",
         date_format = "YYYY-MM-DD",
-        on_or_after = "study_start",
+        on_or_after = "studystart_date",
         return_expectations = {
-            "date": {"earliest": "study_start"},
+            "date": {"earliest": "studystart_date"},
             "incidence": 0.9
         },
     ),
@@ -94,3 +86,4 @@ study = StudyDefinition(
         },
     ),
 )
+  
