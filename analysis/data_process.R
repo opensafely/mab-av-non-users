06b40065bca605c570a78f5e567a5559e47dcb41
Linda Nab <linda.nab@thedatalab.org>
2022-06-21 23:40:11 +0100

    fix error and restrict data extract to columns needed

--- analysis/data_process.R
+++ analysis/data_process.R
@@ -21,6 +21,8 @@ sessionInfo()
 ## Print variable names
 cat("#### read in data extract ####\n")
 ## Read in data and set variable types
+data <- read_csv(here::here("output", "input.csv.gz"))
+
 data_extract <- read_csv(
   here::here("output", "input.csv.gz"),
   col_types = cols_only(
@@ -30,17 +32,12 @@ data_extract <- read_csv(
     
     # POPULATION ----
     age = col_integer(),
-    has_died = col_logical() , 
     sex = col_character(),
-    imd = col_character(),
+    ethnicity = col_character(),
+    imdQ5 = col_character(),
+    region_nhs = col_character(),
     stp = col_character(),
-    registered_eligible = col_logical(),
-    covid_positive_prev_90_days =col_logical(),
-    any_covid_hosp_prev_90_days = col_logical(),
-    high_risk_group = col_logical(),
-    covid_test_positive = col_logical(),
-    prev_treated = col_logical(),
-    high_risk_group = col_logical(),
+    rural_urban = col_character(),
     
     # MAIN ELIGIBILITY - FIRST POSITIVE SARS-CoV-2 TEST IN PERIOD ----
     covid_test_positive_date = col_date(format = "%Y-%m-%d"),
@@ -54,14 +51,11 @@ data_extract <- read_csv(
     date_treated = col_date(format = "%Y-%m-%d"),
     
     # PREVIOUS TREATMENT - NEUTRALISING MONOCLONAL ANTIBODIES OR ANTIVIRALS ----
-    date_treated = col_date(format = "%Y-%m-%d"),
-    prev_treated = col_logical(),
     
     # OVERALL ELIGIBILITY CRITERIA VARIABLES ----
-    covid_test_positive_date2 = col_date(format = "%Y-%m-%d"),
-    symptomatic_covid_test = col_character(),
-    pregnancy =col_logical() , 
+    symptomatic_covid_test = col_date(format = "%Y-%m-%d"),
     covid_symptoms_snomed = col_date(format = "%Y-%m-%d"),
+    pregnancy = col_logical(),
     
     # CENSORING ----
     death_date = col_date(format = "%Y-%m-%d"),
@@ -69,83 +63,70 @@ data_extract <- read_csv(
     
     # HIGH RISK GROUPS ----
     high_risk_cohort_covid_therapeutics = col_character(),
-
-    huntingtons_disease_nhsd =col_logical() , 
-    myasthenia_gravis_nhsd =col_logical() , 
-    motor_neurone_disease_nhsd =col_logical() , 
-    multiple_sclerosis_nhsd =col_logical() , 
-    solid_organ_transplant_nhsd =col_logical() , 
-    hiv_aids_nhsd =col_logical() , 
-    immunosupression_nhsd =col_logical() , 
-    imid_nhsd =col_logical() , 
-    liver_disease_nhsd =col_logical() , 
-    ckd_stage_5_nhsd =col_logical() , 
-    haematological_disease_nhsd =col_logical() , 
-    cancer_opensafely_snomed =col_logical() , 
-    downs_syndrome_nhsd  =col_logical() , 
+    huntingtons_disease_nhsd = col_logical() , 
+    myasthenia_gravis_nhsd = col_logical() , 
+    motor_neurone_disease_nhsd = col_logical() , 
+    multiple_sclerosis_nhsd = col_logical() , 
+    solid_organ_transplant_nhsd = col_logical(), 
+    hiv_aids_nhsd = col_logical(), 
+    immunosupression_nhsd = col_logical(), 
+    imid_nhsd = col_logical(), 
+    liver_disease_nhsd = col_logical(), 
+    ckd_stage_5_nhsd = col_logical(), 
+    haematological_disease_nhsd = col_logical(), 
+    cancer_opensafely_snomed = col_logical(), 
+    downs_syndrome_nhsd  = col_logical(), 
 
     # CLINICAL/DEMOGRAPHIC COVARIATES ----
-    diabetes = col_logical(),
+    diabetes = col_character(),
     bmi = col_character(),
     smoking_status = col_character(),
-    sex = col_character(),
-    ethnicity_primis = col_double(),
-    ethnicity_sus = col_double(),
-    imd = col_double(),
-    region_nhs = col_character(),
-    stp = col_character(),
-    rural_urban = col_character(),
+    copd = col_logical(),
+    dialysis = col_logical(),
+    cancer = col_logical(),
+    lung_cancer = col_logical(),
+    haem_cancer = col_logical(),
+    
+    # VACCINATION ----
     vaccination_status = col_character(),
     date_most_recent_cov_vac = col_date(format = "%Y-%m-%d"),
     pfizer_most_recent_cov_vac = col_logical(),
     az_most_recent_cov_vac = col_logical(),
     moderna_most_recent_cov_vac = col_logical(),
+    
+    # VARIANT ----
     sgtf = col_character(),
     variant = col_character(),
     
     # OUTCOMES ----
-    covid_positive_test_30_days_post_elig_or_treat = col_date(format = "%Y-%m-%d"),
-    covid_hosp_outcome_date0 = col_date(format = "%Y-%m-%d"),
-    covid_hosp_outcome_date1 = col_date(format = "%Y-%m-%d"),
-    covid_hosp_outcome_date2 = col_date(format = "%Y-%m-%d"),
-    covid_hosp_discharge_date0 = col_date(format = "%Y-%m-%d"),
-    covid_hosp_discharge_date1 = col_date(format = "%Y-%m-%d"),
-    covid_hosp_discharge_date2 = col_date(format = "%Y-%m-%d"),
-    covid_hosp_outcome_day_date0 = col_date(format = "%Y-%m-%d"),
-    covid_hosp_outcome_day_date1 = col_date(format = "%Y-%m-%d"),
-    covid_hosp_outcome_day_date2 = col_date(format = "%Y-%m-%d"),
-    covid_hosp_discharge_day_date0 = col_date(format = "%Y-%m-%d"),
-    covid_hosp_discharge_day_date2 = col_date(format = "%Y-%m-%d"),
-    covid_hosp_date_emergency0 =col_date(format = "%Y-%m-%d"),
-    covid_hosp_date_emergency1 = col_date(format = "%Y-%m-%d"),
-    covid_hosp_date_emergency2 = col_date(format = "%Y-%m-%d"),
-    covid_emerg_discharge_date0 = col_date(format = "%Y-%m-%d"),
-    covid_emerg_discharge_date1 = col_date(format = "%Y-%m-%d"),
-    covid_emerg_discharge_date2 = col_date(format = "%Y-%m-%d"),
+    # covid specific
+    covid_hosp_admission_date0 = col_date(format = "%Y-%m-%d"),
+    covid_hosp_admission_date1 = col_date(format = "%Y-%m-%d"),
+    covid_hosp_admission_date2 = col_date(format = "%Y-%m-%d"),
+    covid_hosp_admission_date3 = col_date(format = "%Y-%m-%d"),
+    covid_hosp_admission_date4 = col_date(format = "%Y-%m-%d"),
+    covid_hosp_admission_date5 = col_date(format = "%Y-%m-%d"),
+    covid_hosp_admission_date6 = col_date(format = "%Y-%m-%d"),
+    covid_hosp_admission_first_date7_27 = col_date(format = "%Y-%m-%d"),
+    covid_hosp_discharge_first_date0_7 = col_date(format = "%Y-%m-%d"),
     covid_hosp_date_mabs_procedure = col_date(format = "%Y-%m-%d"),
-    covid_hosp_date_mabs_not_pri = col_date(format = "%Y-%m-%d"),
-    covid_hosp_date_mabs_day = col_date(format = "%Y-%m-%d"),
-    covid_hosp_date0_not_primary = col_date(format = "%Y-%m-%d"),
-    covid_hosp_date1_not_primary = col_date(format = "%Y-%m-%d"),
-    covid_hosp_date2_not_primary =col_date(format = "%Y-%m-%d"),
-    covid_discharge_date0_not_pri = col_date(format = "%Y-%m-%d"),
-    covid_discharge_date1_not_pri =col_date(format = "%Y-%m-%d"),
-    covid_discharge_date2_not_pri = col_date(format = "%Y-%m-%d"),
-    death_with_covid_on_the_death_certificate_date = col_date(format = "%Y-%m-%d"),
-    death_with_covid_underlying_date = col_date(format = "%Y-%m-%d"),
-    hospitalisation_outcome_date0 = col_date(format = "%Y-%m-%d"),
-    hospitalisation_outcome_date1 = col_date(format = "%Y-%m-%d"),
-    hospitalisation_outcome_date2 = col_date(format = "%Y-%m-%d"),
-    hosp_discharge_date0 = col_date(format = "%Y-%m-%d"),
-    hosp_discharge_date1 = col_date(format = "%Y-%m-%d"),
-    hosp_discharge_date2 = col_date(format = "%Y-%m-%d"),
-    covid_hosp_date_mabs_all_cause = col_date(format = "%Y-%m-%d"),
-    
+    # all cause
+    allcause_hosp_admission_date0 = col_date(format = "%Y-%m-%d"),
+    allcause_hosp_admission_date1 = col_date(format = "%Y-%m-%d"),
+    allcause_hosp_admission_date2 = col_date(format = "%Y-%m-%d"),
+    allcause_hosp_admission_date3 = col_date(format = "%Y-%m-%d"),
+    allcause_hosp_admission_date4 = col_date(format = "%Y-%m-%d"),
+    allcause_hosp_admission_date5 = col_date(format = "%Y-%m-%d"),
+    allcause_hosp_admission_date6 = col_date(format = "%Y-%m-%d"),
+    allcause_hosp_admission_first_date7_27 = col_date(format = "%Y-%m-%d"),
+    allcause_hosp_discharge_first_date0_7 = col_date(format = "%Y-%m-%d"),
+    allcause_hosp_date_mabs_procedure = col_date(format = "%Y-%m-%d"),
+    # death
+    died_ons_covid_any_date = col_date(format = "%Y-%m-%d")
 ),
   na = character(), # more stable to convert to missing later
 )
 
-## Parse NAs
 data_extract2 <- data_extract %>%
   mutate(across(
     .cols = where(is.character),
@@ -176,8 +157,6 @@ data_processed <- data_extract2 %>%
       TRUE ~ NA_character_
     ),
     
-    ethnicity = coalesce(ethnicity_primis, ethnicity_sus),
-    
     ethnicity = fct_case_when(
       ethnicity == "1" ~ "White",
       ethnicity == "2" ~ "Mixed",
@@ -195,13 +174,13 @@ data_processed <- data_extract2 %>%
       #TRUE ~ "Unknown"
       TRUE ~ NA_character_),
     
-    imd = fct_case_when(
-      imd == "5" ~ "5 (least deprived)",
-      imd == "4" ~ "4",
-      imd == "3" ~ "3",
-      imd == "2" ~ "2",
-      imd == "1" ~ "1 (most deprived)",
-      imd == "0" ~ NA_character_
+    imdQ5 = fct_case_when(
+      imdQ5 == "5 (least deprived)" ~ "5 (least deprived)",
+      imdQ5 == "4" ~ "4",
+      imdQ5 == "3" ~ "3",
+      imdQ5 == "2" ~ "2",
+      imdQ5 == "1 (most deprived)" ~ "1 (most deprived)",
+      TRUE ~ NA_character_
     ),
   
     region_nhs = fct_case_when(
@@ -235,10 +214,10 @@ data_processed <- data_extract2 %>%
     treat_window = covid_test_positive_date + days(5),
     
     # Day of treatment
-    tb_postest_treat = ifelse(covid_test_positive == 1, as.numeric(date_treated - covid_test_positive_date), NA),
+    tb_postest_treat = ifelse(!is.na(date_treated), difftime(date_treated, covid_test_positive_date), NA),
     
     # Flag records where treatment date falls in treatment assignment window
-    treat_check = ifelse(date_treated >= covid_test_positive & date_treated <= treat_window, 1, 0),
+    treat_check = ifelse(date_treated >= covid_test_positive_date & date_treated <= treat_window, 1, 0),
     
     # Treatment strategy sep
     treatment_strategy_sep = case_when(
@@ -289,63 +268,7 @@ print(dim(data_processed))
 cat("#### data_processed_eligible ####\n")
 print(dim(data_processed_eligible))
 
-## Clean data
-data_processed_clean <- data_processed_eligible %>% 
-  select(
-    
-    # Identifier
-    patient_id,
-    
-    # POPULATION ----
-    age, has_died, sex, imd, stp, registered_eligible, covid_positive_prev_90_days,
-    any_covid_hosp_prev_90_days, high_risk_group, covid_test_positive, prev_treated, high_risk_group ,
-    
-    # MAIN ELIGIBILITY - FIRST POSITIVE SARS-CoV-2 TEST IN PERIOD ----
-    covid_test_positive_date, 
-    
-    # TREATMENT - NEUTRALISING MONOCLONAL ANTIBODIES OR ANTIVIRALS ---
-    treat_window, tb_postest_treat ,treat_check , treatment_strategy_sep, treatment_strategy_overall , date_treated ,
-   
-    # OVERALL ELIGIBILITY CRITERIA VARIABLES ----
-    covid_test_positive_date2, symptomatic_covid_test, pregnancy, covid_symptoms_snomed,
-    
-    # CENSORING ----
-    death_date,dereg_date ,
-    
-    # HIGH RISK GROUPS ----
-    high_risk_cohort_covid_therapeutics ,
-    huntingtons_disease_nhsd , 
-    myasthenia_gravis_nhsd  , 
-    motor_neurone_disease_nhsd,
-    multiple_sclerosis_nhsd , 
-    solid_organ_transplant_nhsd , 
-    hiv_aids_nhsd , 
-    immunosupression_nhsd = , 
-    imid_nhsd  , 
-    liver_disease_nhsd  , 
-    ckd_stage_5_nhsd  , 
-    haematological_disease_nhsd , 
-    cancer_opensafely_snomed  , 
-    downs_syndrome_nhsd  , 
-    
-    # CLINICAL/DEMOGRAPHIC COVARIATES ----
-    diabetes  ,
-    bmi  ,
-    smoking_status ,
-    sex ,
-    ethnicity  ,
-    imd  ,
-    region_nhs ,
-    stp  ,
-    rural_urban  ,
-    vaccination_status  ,
-    date_most_recent_cov_vac  ,
-    pfizer_most_recent_cov_vac,
-    az_most_recent_cov_vac ,
-    moderna_most_recent_cov_vac ,
-    sgtf ,
-    variant,
-)
 # save data
-write_rds(data_processed_clean, here::here("output", "data", "data_processed.rds"), compress = "gz")
-write_csv(data_processed_clean, here::here("output", "data", "data_processed.csv"))
+fs::dir_create(here::here("output", "data"))
+write_rds(data_processed, here::here("output", "data", "data_processed.rds"), compress = "gz")
+write_csv(data_processed_eligible, here::here("output", "data", "data_processed.csv"))
