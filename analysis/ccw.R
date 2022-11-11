b3d7129362f8d3feb925fc669b233ce9eca49461
Linda Nab <linda.nab@thedatalab.org>
2022-11-11 10:23:26 +0100

    split survival data

--- analysis/ccw.R
+++ analysis/ccw.R
@@ -62,29 +62,50 @@ if (length(args) == 0){
 data_filename <-
   paste0(period[!period == "ba1"], "_"[!period == "ba1"],
          "data_processed_", "day0", ".rds")
-data_processed <-
+data <-
   read_rds(here::here("output", "data", data_filename))
 
 ################################################################################
 # 0.4 Prepare data
 ################################################################################
-# This step simplifies the variable 'status_primary_simple'
+# This step 
+# 1. simplifies the variable 'status_primary'
 # --> no distinction is made between censored because dereg or because
 #     noncovid_death occurred
-# --> levels are 0 (no event/censored); 1 (covid hosp/death) and 2 (censored)
+# --> levels are 0 (no event/censored); 1 (covid hosp/death)
 # FIX ME:
 # - Should we treat non covid death as a competing risk? (..somehow) 
 #   (= informative censoring)
 # - Should we censor for non covid hosp? --> currently ignored
-data_processed <-
-  data_processed %>%
+# 2. simplifies the variable 'any_treatment_strategy_cat'
+# --> this variable is equal to 'Untreated', 'Sotrovimab' or 'Molnupiravir', 
+#     depending on whether patients has been treated or not during follow up 
+# --> people are sometimes treated after experiencing a primary event
+# --> we classify people as untreated if they experience an event and set their
+#     tb_postest_treat to NA (= time between treatment and day 0)
+data <-
+  data %>%
   mutate(
     status_primary_simple = case_when(
-      status_primary %in% c("dereg", "noncovid_death") ~ "2",
+      status_primary %in% c("none", "dereg", "noncovid_death") ~ "0",
       status_primary == "covid_hosp_death" ~ "1",
-      status_primary == "none" ~ "0",
       TRUE ~ status_primary %>% as.character(),
-      ) %>% factor(levels = c("0", "1", "2")),
+      ) %>% factor(levels = c("0", "1")),
+    # in the data, people sometimes go on treatment after experiencing an event
+    # (censoring event or outcome event)
+    # --> these patient's treatment is set to untreated
+    any_treatment_strategy_cat = case_when(
+      # if on same day, we assume treatment was before event
+      fu_primary < tb_postest_treat ~ "Untreated" %>%
+        factor(levels = c("Untreated", "Molnupiravir", "Sotrovimab")),
+      TRUE ~ any_treatment_strategy_cat,
+    ),
+    # if people are untreated following the above defined rule, set 
+    # tb_postest_treat (day of fup on wich they've been treated to NA)
+    tb_postest_treat = case_when(
+      any_treatment_strategy_cat == "Untreated" ~ NA_real_,
+      TRUE ~ tb_postest_treat,
+    ),
   )
 
 ################################################################################
@@ -103,8 +124,8 @@ data_processed <-
 ###################################################
 # ARM CONTROL: no treatment within 5 days
 ###################################################
-data_processed_control <- 
-  data_processed %>%
+data_control <- 
+  data %>%
   mutate(arm = "Control",
          # ADD VARIABLES OUTCOME AND FUP
          outcome = case_when(
@@ -112,7 +133,7 @@ data_processed_control <-
            # --> they are still alive and followed up until treatment
            any_treatment_strategy_cat != "Untreated" &
              tb_postest_treat <= 4 ~ "0" %>% 
-             factor(levels = c("0", "1", "2")),
+             factor(levels = c("0", "1")),
            # Case 2: patients do not receive treatment within 5 days (scenarios F to M)
            # [either no treatment or treatment after five days]
            # --> we keep their observed outcomes and follow-up times
@@ -151,9 +172,8 @@ data_processed_control <-
            (any_treatment_strategy_cat == "Untreated" &
              fu_primary > 4) |
              (any_treatment_strategy_cat != "Untreated" &
-             tb_postest_treat > 4 & 
-               # FIX ME: could people receive treatment after our outcome/censored?
-               fu_primary > 4) ~ "0" %>% factor(levels = c("0", "1")),
+             # NB if people treated > 4 days, fu_primary is > 4 days
+             tb_postest_treat > 4) ~ "0" %>% factor(levels = c("0", "1")),
            ),
          fup_uncensored = case_when(
            # Case 1: Patients receive treatment within 5 days (scenarios A to E)
@@ -172,16 +192,15 @@ data_processed_control <-
            (any_treatment_strategy_cat == "Untreated" &
               fu_primary > 4) |
              (any_treatment_strategy_cat == "Treated" &
-                tb_postest_treat > 4 &
-                # FIX ME: could people receive treatment after our outcome/censored?
-                fu_primary > 4) ~ 5,
+                # NB if people treated > 4 days, fu_primary is > 4 days
+                tb_postest_treat > 4) ~ 5,
            ),
          )
 ###################################################
 # ARM TREATMENT: treatment within 5 days
 ###################################################
-data_processed_treatment <- 
-  data_processed %>%
+data_trt <- 
+  data %>%
   mutate(arm = "Treatment",
          # ADD VARIABLES OUTCOME AND FUP
          outcome = case_when(
@@ -193,31 +212,33 @@ data_processed_treatment <-
            # without being treated (scenarios K and L)
            # --> we keep their observed outcomes and follow-up times
            any_treatment_strategy_cat == "Untreated" &
-             fu_all <= 4 ~ status_primary_simple,
+             fu_primary <= 4 ~ status_primary_simple,
            # Case 3: Patients do not receive treatment within 5 days
            # and are still alive or at risk at 5 days (scenarios F-J and M)
-           # --> they are considered alive and their follow-up time is 5 days
+           # --> they don't experience an event and their follow-up time is 5 
+           #     days
            (any_treatment_strategy_cat == "Untreated" &
-             fu_all > 4) |
+             fu_primary > 4) |
              (any_treatment_strategy_cat != "Untreated" & 
-                tb_postest_treat > 4) ~ "none" %>% 
-             factor(levels = c("none", "censored", "covid_hosp_death")),
+                tb_postest_treat > 4) ~ "0" %>% 
+             factor(levels = c("0", "1", "2")),
          ),
          fup = case_when(
            # Case 1: Patients receive treatment within 5 days (scenarios A to E)
            # --> we keep their observed outcomes and follow-up times
            any_treatment_strategy_cat != "Untreated" &
-             tb_postest_treat <= 4 ~ fu_primary_simple,
+             tb_postest_treat <= 4 ~ fu_primary,
            # Case 2: Patients die or are lost to follow-up within 5 days
            # without being treated (scenarios K and L)
            # --> we keep their observed outcomes and follow-up times
            any_treatment_strategy_cat == "Untreated" &
-             fu_primary_simple <= 4 ~ fu_primary_simple,
+             fu_primary <= 4 ~ fu_primary,
            # Case 3: Patients do not receive treatment within 5 days
            # and are still alive or at risk at 5 days (scenarios F-J and M)
-           # --> they are considered alive and their follow-up time is 5 days
+           # --> they don't experience an event and their follow-up time is 5 
+           #     days
            (any_treatment_strategy_cat == "Untreated" &
-              fu_primary_simple > 4) |
+              fu_primary > 4) |
              (any_treatment_strategy_cat != "Untreated" & 
                 tb_postest_treat > 4) ~ 5,
          ),
@@ -225,49 +246,51 @@ data_processed_treatment <-
          censoring = case_when(
            # Case 1: Patients receive treatment within 5 days (scenarios A to E): 
            # --> they are uncensored in the treatment arm and remain at risk of 
-           # censoring until time of treatment
+           #     censoring until time of treatment
            any_treatment_strategy_cat != "Untreated" & 
-             tb_postest_treat <= 4 ~ "0",
+             tb_postest_treat <= 4 ~ "0" %>% 
+             factor(levels = c("0", "1")),
            # Case 2: Patients die or are lost to follow-up within 5 days
            # without being treated (scenarios K and L)
            # --> we keep their follow-up times but they are uncensored
            any_treatment_strategy_cat == "Untreated" & 
-             fu_all <= 4 ~ "0",
+             fu_primary <= 4 ~ "0" %>%
+             factor(levels = c("0", "1")),
            # Case 3: Patients do not receive treatment within 5 days and are 
            # still alive or at risk at 5 days (scenarios F-J and M): 
            # --> they are considered censored and their follow-up time is 5 days
            (any_treatment_strategy_cat == "Untreated" &
-             fu_primary_simple > 4) | 
+             fu_primary > 4) | 
              (any_treatment_strategy_cat != "Untreated" &
-             tb_postest_treat > 4) ~ "1"
+             tb_postest_treat > 4) ~ "1" %>% 
+             factor(levels = c("0", "1")),
          ),
          fup_uncensored = case_when(
            # Case 1: Patients receive treatment within 5 days (scenarios A to E): 
            # --> they are uncensored in the treatment arm and remain at risk of 
-           # censoring until time of treatment
+           #     censoring until time of treatment
            any_treatment_strategy_cat != "Untreated" & 
              tb_postest_treat <= 4 ~ tb_postest_treat,
            # Case 2: Patients die or are lost to follow-up within 5 days
            # without being treated (scenarios K and L)
            # --> we keep their follow-up times but they are uncensored
            any_treatment_strategy_cat == "Untreated" & 
-             fu_all <= 4 ~ fu_all,
+             fu_primary <= 4 ~ fu_primary,
            # Case 3: Patients do not receive treatment within 5 days and are 
            # still alive or at risk at 5 days (scenarios F-J and M): 
            # --> they are considered censored and their follow-up time is 5 days
            (any_treatment_strategy_cat == "Untreated" &
-              fu_primary_simple > 4) | 
+              fu_primary > 4) | 
              (any_treatment_strategy_cat != "Untreated" &
                 tb_postest_treat > 4) ~ 5,
          ),
   )
-data_processed_treatment %>% select(outcome) %>% table()
 ###################################################
 # CREATION OF THE FINAL DATASET FOR THE ANALYSIS
 ###################################################
-data_cohort <-
-  rbind(data_processed_control,
-        data_processed_treatment)
+data_cloned <-
+  rbind(data_control,
+        data_trt)
 
 ################################################################################
 # STEP 2-SPLITTING THE DATASET AT EACH TIME OF EVENT
@@ -275,88 +298,44 @@ data_cohort <-
 # Dataframe 'times' containing the time of events and an ID for the times of 
 # events
 t_events <- sort(unique(data_cohort$fup))
-times <- data.frame("t_event" = t_events, "ID_t" = seq(1:length(t_events)))
 
-data_processed_treatment <- 
-  data_processed_treatment %>%
-  mutate(Tstart = 0)
-data_processed_treatment_long <-
-  data_processed_treatment %>%
+data_outcome <- 
+  data_cloned %>%
+  mutate(t_start = 0) %>%
   survSplit(cut = t_events,
             end = "fup",
-            start = "Tstart",
-            event = "outcome",
-            id = patient_id)
+            start = "t_start",
+            event = "outcome") %>%
+  mutate(outcome = case_when(
+    outcome == "censor" ~ "0",
+    TRUE ~ "1",
+    ) %>% factor(levels = c("0", "1"))
+  )
+# LN QU: Don't get why we're using fup and not fup_censoring?????
+# fup_censoring is not used in the script...? --> seems odd
+data_cnsr <-
+  data_cloned %>%
+  mutate(t_start = 0) %>%
+  survSplit(cut = t_events,
+            end = "fup",
+            start = "t_start",
+            event = "censoring") %>%
+  mutate(censoring = case_when(
+    censoring == "censor" ~ "0",
+    TRUE ~ "1",
+  ) %>% factor(levels = c("0", "1"))
+  )
+  
+data_final <-
+  data_outcome %>%
+  left_join(data_cnsr,
+            by = c("arm", "patient_id", "fup", "t_start")) %>%
+  select(-c("outcome.y", "censoring.x")) %>%
+  rename(outcome = outcome.x,
+         censoring = censoring.y,
+         t_end = fup)
 
-
-tab_s<-tab[tab$arm=="Surgery",]
-
-#Creation of the entry variable (Tstart, 0 for everyone)
-tab_s$Tstart<-0
-
-#Splitting the dataset at each time of event until the event happens and sorting it
-data.long<-survSplit(tab_s, cut=t_events, end="fup", 
-                     start="Tstart", event="outcome",id="ID") 
-data.long<-data.long[order(data.long$ID,data.long$fup),] 
-
-#Splitting the original dataset at each time of event and sorting it
-#until censoring happens. This is to have the censoring status at each time of event 
-data.long.cens<-survSplit(tab_s, cut=t_events, end="fup", 
-                          start="Tstart", event="censoring",id="ID") 
-data.long.cens<-data.long.cens[order(data.long.cens$ID,data.long.cens$fup),] 
-
-#Replacing the censoring variable in data.long by the censoring variable obtained
-# in the second split dataset
-data.long$censoring<-data.long.cens$censoring
-
-#Creating Tstop (end of the interval) 
-data.long$Tstop<-data.long$fup
-
-#Merge and sort
-data.long<-merge(data.long,times,by.x="Tstart",by.y="tevent",all.x=T)
-data.long<-data.long[order(data.long$ID,data.long$fup),] 
-data.long$ID_t[is.na(data.long$ID_t)]<-0
-
-
-
-####################################
-# ARM "No Surgery" NOW
-###################################
-tab_c<-tab[tab$arm=="Control",]
-
-#Creation of the entry variable (Tstart, 0 for everyone)
-tab_c$Tstart<-0
-
-
-#Splitting the dataset first at each time of event
-#until the event happens 
-data.long2<-survSplit(tab_c, cut=t_events, end="fup", 
-                      start="Tstart", event="outcome",id="ID") 
-data.long2<-data.long2[order(data.long2$ID,data.long2$fup),] 
-
-#Splitting the original dataset at each time of event
-#until censoring happens 
-data.long.cens2<-survSplit(tab_c, cut=t_events, end="fup", 
-                           start="Tstart", event="censoring",id="ID") 
-data.long.cens2<-data.long.cens2[order(data.long.cens2$ID,data.long.cens2$fup),] 
-
-
-#Replacing the censoring variable in data.long by the censoring variable obtained
-# in the second split dataset
-data.long2$censoring<-data.long.cens2$censoring
-
-#Creating Tstop (end of the interval)
-data.long2$Tstop<-data.long2$fup
-
-#Merge and sort
-data.long2<-merge(data.long2,times,by.x="Tstart",by.y="tevent",all.x=T)
-data.long2<-data.long2[order(data.long2$ID,data.long2$fup),] 
-data.long2$ID_t[is.na(data.long2$ID_t)]<-0
-
-#Final dataset
-data<-rbind(data.long,data.long2)
-data_final<-merge(data,times,by="ID_t",all.x=T)
-data_final<-data_final[order(data_final$ID,data_final$fup),]
-
-
-
+# TO DO:
+# - Model weights for censoring (if needed?)
+# - Outcome model
+# - Variance estimation (---> bootstrap?)
