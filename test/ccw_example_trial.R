c50ecb89ad2dcb58f3c61846b55d75d9f51540b7
Linda Nab <linda.nab@thedatalab.org>
2023-01-17 16:05:05 +0000

    add code for pooled logreg

--- test/ccw_example_trial.R
+++ test/ccw_example_trial.R
@@ -11,6 +11,7 @@
 ################################################################################
 library(tidyverse)
 library(survival)
+library(lspline)
 
 ################################################################################
 # 1. Create tibble with trial data
@@ -22,8 +23,10 @@ library(survival)
 # 5 experience an outcome on day 1, 2, 3, 4, or day 22.
 data <- 
   tibble(pt_id = 1:36,
-         outcome = rep(c(1, 1, 1, 1, 1, 0), 6),
-         fup = rep(c(1, 2, 3, 4, 22, 27), 6),
+         outcome = c(c(1, 1, 1, 1, 1, 0),
+                     rep(c(1, 1, 1, 1, 1, 0), 5)),
+         fup = c(c(1, 2, 3, 4, 22, 27),
+                 rep(c(1, 2, 3, 4, 22, 27), 5)),
          treatment = c(rep(0, 6), rep(1, 30)),
          treatment_t = c(rep(NA_real_, 6),
                          rep(0, 6), 
@@ -57,6 +60,20 @@ coxph(Surv(fup, outcome) ~ treatment,
 glm(outcome ~ treatment,
     family = binomial(link = "logit"), 
     data = data) 
+# pooled logistic regression
+cuts <- 1:27
+data_split <-
+  data %>%
+  survSplit(cut = cuts, 
+            start = "start",
+            zero = 0,
+            end = "fup",
+            event = "outcome")
+pooled_logreg_fit <- 
+  glm(outcome ~ treatment + lspline(fup, 1),
+      family = binomial(link = "logit"),
+      data = data_split)
+pooled_logreg_fit$coefficients[2] %>% exp() # approx same as HR
 ################################################################################
 # 2. Create tibble with observed data
 ################################################################################
@@ -88,6 +105,20 @@ coxph(Surv(fup, outcome) ~ treatment_obs,
 glm(outcome ~ treatment_obs,
     family = binomial(link = "logit"), 
     data = data_obs) 
+# pooled logistic regression
+cuts <- 1:27
+data_obs_split <-
+  data_obs %>%
+  survSplit(cut = cuts, 
+            start = "start",
+            zero = 0,
+            end = "fup",
+            event = "outcome")
+pooled_logreg_fit_obs <- 
+  glm(outcome ~ treatment_obs + lspline(fup, 1),
+      family = binomial(link = "logit"),
+      data = data_obs_split)
+pooled_logreg_fit_obs$coefficients[2] %>% exp() # approx same as HR
 
 ################################################################################
 # 3. CCW
@@ -129,6 +160,7 @@ data_notrt <-
          fup_art_uncensored = case_when(art_censoring == 1 ~ treatment_t_obs,
                                         TRUE ~ fup),
          outcome_obs = case_when(outcome == 1 & fup <= fup_art_uncensored ~ 1,
+                                 outcome == 1 & art_censoring == 0 ~ 1,
                                  TRUE ~ 0),
          fup_obs = case_when(outcome_obs == 1 ~ fup,
                              TRUE ~ fup_art_uncensored))
@@ -142,7 +174,7 @@ data_trt <-
 # prepare data
 data_trt <- 
   data_trt %>%
-  mutate(art_censoring = case_when(treatment_obs == 0 & fup > 4 ~ 1,
+  mutate(art_censoring = case_when(treatment_obs == 0 & fup >= 4 ~ 1,
                                    TRUE ~ 0), # art censored if not treated and
                                               # follow-up longer than 4 days
                                               # if outcome before or on day 4,
@@ -150,16 +182,18 @@ data_trt <-
                                               # art censored
          fup_art_uncensored = case_when(art_censoring == 1 ~ 4,
                                         TRUE ~ fup),
-         outcome_obs = case_when(outcome == 1 & fup <= fup_art_uncensored ~ 1,
+         outcome_obs = case_when(outcome == 1 & fup < fup_art_uncensored ~ 1,
+                                 outcome == 1 & art_censoring == 0 ~ 1,
                                  TRUE ~ 0),
          fup_obs = case_when(outcome_obs == 1 ~ fup,
                              TRUE ~ fup_art_uncensored))
 
 ################################################################################
-# 3.2 STEP 3: SPLIT DATASET AT EACH DAY
+# 3.2A STEP 3: SPLIT DATASET AT EACH DAY & CALCULAT£ WEIGHTS NO TREATMENT ARM
 ################################################################################
 cuts <- 1:27
 # Prepare data for the calculation of weights
+# No of people artificially censored per time interval
 split_notrt_art_censoring <- 
   data_notrt %>%
     survSplit(data = .,
@@ -172,6 +206,31 @@ split_notrt_art_censoring <-
     summarise(sum_art_censored = sum(art_censoring),
               n = n(),
               .groups = "drop")
+split_notrt_art_censoring <- 
+  data_notrt %>%
+  survSplit(data = .,
+            cut = cuts, 
+            start = "start",
+            zero = 0,
+            end = "fup_art_uncensored",
+            event = "art_censoring")
+fit_artc <-
+  glm(art_censoring ~ fup_art_uncensored,
+      family = binomial(link = "logit"),
+      data = split_notrt_art_censoring)
+1 / (1 - predict(fit_artc, type = "response"))
+data_notrt_split <-
+  data_notrt_split %>%
+  mutate(p_c = predict(fit_artc,
+                       data = data_notrt_split))
+data_notrt_split %>% View()
+
+pooled_logreg_fit_obs_notrt <- 
+  glm(censoring ~ lspline(end, 1),
+      family = binomial(link = "logit"),
+      data = data_notrt_split)
+
+# No of people experiencing an outcome per time interval
 split_notrt_outcome <- 
   data_notrt %>%
     survSplit(data = .,
@@ -184,18 +243,16 @@ split_notrt_outcome <-
     summarise(sum_outcome = sum(outcome_obs),
               n = n(),
               .groups = "drop")
+# Calculate weights
 weights_notrt <-
   split_notrt_art_censoring %>%
   left_join(split_notrt_outcome,
             by = c("start", "fup_art_uncensored" = "fup_obs", "n")) %>%
   mutate(p_uncensored = (n - sum_art_censored) / (n),
-         weight = 1 / p_uncensored)
-weights_notrt <-
-  weights_notrt %>%
+         weight = 1 / p_uncensored) %>%
   rename(end = fup_art_uncensored) %>%
   select(start, end, weight) %>%
   mutate(cum_weight = cumprod(weight))
-  
 # Split data set and add weights to data
 data_notrt_split <- 
   data_notrt %>%
@@ -204,11 +261,65 @@ data_notrt_split <-
               start = "start",
               zero = 0,
               end = "fup_obs",
-              event = "outcome_obs")
-data_notrt_split <- 
-  data_notrt_split %>%
+              event = "outcome_obs") %>%
   full_join(weights_notrt,
             by = c("start" = "start", "fup_obs" = "end"))
+################################################################################
+# 3.2B STEP 3: SPLIT DATASET AT EACH DAY & CALCULAT£ WEIGHTS TREATMENT ARM
+################################################################################
+cuts <- 1:27
+# Prepare data for the calculation of weights
+# No of people artificially censored per time interval
+split_trt_art_censoring <- 
+  data_trt %>%
+  survSplit(data = .,
+            cut = cuts, 
+            start = "start",
+            zero = 0,
+            end = "fup_art_uncensored",
+            event = "art_censoring") %>%
+  group_by(start, fup_art_uncensored) %>%
+  summarise(sum_art_censored = sum(art_censoring),
+            n = n(),
+            .groups = "drop")
+# No of people experiencing an outcome per time interval
+split_trt_outcome <- 
+  data_trt %>%
+  survSplit(data = .,
+            cut = cuts,
+            start = "start",
+            zero = 0,
+            end = "fup_obs",
+            event = "outcome_obs") %>%
+  group_by(start, fup_obs) %>%
+  summarise(sum_outcome = sum(outcome_obs),
+            n = n(),
+            .groups = "drop")
+# Calculate weights
+weights_trt <-
+  split_trt_art_censoring %>%
+  left_join(split_trt_outcome,
+            by = c("start", "fup_art_uncensored" = "fup_obs", "n")) %>%
+  mutate(p_uncensored = (n - sum_art_censored) / (n),
+         weight = 1 / p_uncensored) %>%
+  rename(end = fup_art_uncensored) %>%
+  select(start, end, weight) %>%
+  mutate(cum_weight = cumprod(weight))
+# Split data set and add weights to data
+data_trt_split <- 
+  data_trt %>%
+  survSplit(data = .,
+            cut = cuts,
+            start = "start",
+            zero = 0,
+            end = "fup_obs",
+            event = "outcome_obs") %>%
+  full_join(weights_trt,
+            by = c("start" = "start", "fup_obs" = "end"))
+
+
+
+
 
 survfit(Surv(start, fup_obs, outcome_obs == 1) ~ 1, 
         data_notrt_split, 
@@ -216,15 +327,37 @@ survfit(Surv(start, fup_obs, outcome_obs == 1) ~ 1,
 survfit(Surv(start, fup_obs, outcome_obs == 1) ~ 1, 
         data_notrt_split) %>% summary()
 
+survfit(Surv(start, fup_obs, outcome_obs == 1) ~ 1, 
+        data_trt_split, 
+        weights = cum_weight) %>% summary()
+survfit(Surv(start, fup_obs, outcome_obs == 1) ~ 1, 
+        data_trt_split) %>% summary()
 
-glm(outcome_obs ~ 1,
-    family = binomial(link = "logit"),
-    data = data_notrt_split)
 
-glm(outcome_obs ~ 1, 
+data_ccw <-
+  rbind(data_notrt_split,
+        data_trt_split)
+data_ccw %>%
+  group_by(arm, outcome_obs) %>%
+  tally()
+
+pooled_logreg_fit_ccw <- 
+  glm(outcome_obs ~ arm + lspline(fup_obs, 1), 
+      family = binomial(link = "logit"),
+      weights = cum_weight,
+      data = data_ccw)
+pooled_logreg_fit_ccw$coefficients[2] %>% exp()
+
+coxph(Surv(start, fup_obs, outcome_obs == 1) ~ arm,
+      data = data_ccw,
+      weights = cum_weight)
+
+
+
+glm(outcome_obs ~ arm,
     family = binomial(link = "logit"),
-    weights = cum_weight,
-    data = data_notrt_split)
+    data = data_ccw)
+library(rms)
 
 
 
