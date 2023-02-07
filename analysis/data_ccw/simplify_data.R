de42ff7de0277045a6170e49b5b4c809da31e79b
Linda Nab <linda.nab@thedatalab.org>
2023-02-07 22:34:45 +0100

    add ccw analysis

---
+++ analysis/data_ccw/simplify_data.R
@@ -0,0 +1,52 @@
+ccw_simplify_data <- function(data, outcome, contrast){
+  if (outcome == "primary"){
+    data <-
+      data %>%
+      mutate(
+        # simplify primary status
+        status_ccw_simple = if_else(
+          status_primary %in% c("covid_hosp_death"),
+          1,
+          0),
+        # some people have been treated on or after they experience an event,
+        # variable 'treatment_prim' is then 'Untreated' (see process_data.R), if so,
+        # set tb_postest_treat (day of fup on which they've been treated to NA)
+        tb_postest_treat_ccw = if_else(
+          treatment_prim == "Untreated",
+          NA_real_,
+          tb_postest_treat),
+        treatment_ccw = treatment_prim,
+        fu_ccw = fu_primary,
+      )
+    if (contrast != "all"){
+      data <- 
+        data %>%
+        filter(treatment_strategy_cat_prim %in% c("Untreated", contrast))
+    }
+  } else if (outcome == "secondary"){
+    data <-
+      data %>%
+      mutate(
+        # simplify primary status
+        status_ccw_simple = if_else(
+          status_secondary %in% c("allcause_hosp_death"),
+          1,
+          0),
+        # some people have been treated on or after they experience an event,
+        # variable 'treatment_prim' is then 'Untreated' (see process_data.R), if so,
+        # set tb_postest_treat (day of fup on which they've been treated to NA)
+        tb_postest_treat_ccw = if_else(
+          treatment_sec == "Untreated",
+          NA_real_,
+          tb_postest_treat),
+        treatment_ccw = treatment_sec,
+        fu_ccw = fu_secondary,
+      )
+    if (contrast != "all"){
+      data <- 
+        data %>%
+        filter(treatment_strategy_cat_sec %in% c("Untreated", contrast))
+    }
+  }
+  data
+}
