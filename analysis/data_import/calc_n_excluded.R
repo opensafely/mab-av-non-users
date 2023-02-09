c510ac1acae0ae4523c0cbd8ac77d5b33147a1ac
Linda Nab <linda.nab@thedatalab.org>
2023-02-09 14:10:44 +0100

    add total included

--- analysis/data_import/calc_n_excluded.R
+++ analysis/data_import/calc_n_excluded.R
@@ -20,8 +20,20 @@ calc_n_excluded <- function(data_processed){
     filter(!is.na(paxlovid_covid_therapeutics) |
              !is.na(remdesivir_covid_therapeutics)) %>%
     nrow()
+  n_after_exclusion_processing <- 
+    data_processed %>%
+    # Exclude patients treated with both sotrovimab and molnupiravir on the
+    # same day 
+    filter(treated_sot_mol_same_day  == 0) %>%
+    # Exclude patients hospitalised on day of positive test
+    filter(!(status_all %in% c("covid_hosp", "noncovid_hosp") &
+               fu_all == 0)) %>%
+    # if treated with paxlovid or remidesivir --> exclude
+    filter(is.na(paxlovid_covid_therapeutics) &
+             is.na(remdesivir_covid_therapeutics)) %>% nrow()
   out <- tibble(n_before_exclusion_processing,
                 n_treated_same_day,
                 n_hospitalised_pos_test,
-                n_treated_pax_rem)
+                n_treated_pax_rem,
+                n_after_exclusion_processing)
 }
