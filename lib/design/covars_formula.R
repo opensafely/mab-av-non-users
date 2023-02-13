82d544c6043e0f0a5e0029696b44a4e3fbd9eb40
Linda Nab <linda.nab@thedatalab.org>
2023-02-13 19:00:26 +0100

    add tb postest vax as a cont var with spline

--- lib/design/covars_formula.R
+++ lib/design/covars_formula.R
@@ -35,10 +35,7 @@ covars_formula <-
     "huntingtons_disease_nhsd",
     # vax vars
     "vaccination_status",
-    "tb_postest_vax_7",
-    "tb_postest_vax_7_27",
-    "tb_postest_vax_28_83",
-    "tb_postest_vax_84",
+    "ns(tb_postest_vacc, df = 3)",
     "pfizer_most_recent_cov_vac",
     "az_most_recent_cov_vac",
     "moderna_most_recent_cov_vac",
