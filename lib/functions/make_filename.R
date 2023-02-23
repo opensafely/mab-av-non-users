64975ab3c59e67483410b81eb2ed073d93c44ee1
Linda Nab <linda.nab@thedatalab.org>
2023-02-23 18:50:07 +0000

    add folder structure for additional analyses

--- lib/functions/make_filename.R
+++ lib/functions/make_filename.R
@@ -1,11 +1,26 @@
-make_filename <- function(object_name, period, outcome, contrast, type){
-  paste0(period[period != "ba1"],
-         "_"[period != "ba1"],
-         object_name,
-         "_",
-         contrast %>% tolower(),
-         "_"[outcome != "primary"],
-         outcome[outcome != "primary"],
-         ".",
-         type)
+make_filename <- function(object_name, period, outcome, contrast, subgrp, supp, type){
+  file_name <- 
+    paste0(
+      period[period != "ba1"],
+      "_"[period != "ba1"],
+      object_name,
+      "_",
+      contrast %>% tolower(),
+      "_"[outcome != "primary"],
+      outcome[outcome != "primary"],
+      "_"[subgrp != "full"],
+      subgrp[subgrp != "full"],
+      "_"[supp != "main"],
+      supp[supp != "main"],
+      ".",
+      type)
+  if (supp != "main"){
+    file_name <- 
+      fs::path(supp, file_name)
+  }
+  if(subgrp != "full"){
+    file_name <-
+      fs::path(subgrp, file_name)
+  }
+  file_name
 }
