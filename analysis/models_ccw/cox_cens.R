e8a78c0c235e4bed3d8c54bf89edb3ed408dae58
Linda Nab <linda.nab@thedatalab.org>
2023-03-02 23:06:11 +0000

    set singular.ok to FALSE

--- analysis/models_ccw/cox_cens.R
+++ analysis/models_ccw/cox_cens.R
@@ -13,6 +13,7 @@ fit_cens_cox <- function(data_long, formula_cens){
     coxph(formula_cens,
           ties = "efron",
           data = data_long,
+          singular.ok = FALSE,
           model = TRUE)
 }
 basehaz_cens <- function(cox_fit){
