080231b53b8cf2e9567a900c4fad0435db6b7080
Linda Nab <linda.nab@thedatalab.org>
2023-03-03 14:41:54 +0000

    use tstart not fup

--- analysis/models_ccw/plr_cens.R
+++ analysis/models_ccw/plr_cens.R
@@ -1,7 +1,7 @@
 # PLR model
 create_formula_cens_plr <- function(covars_formula){
   formula_cens <- paste0("censoring ~ ",
-                         paste0(c("poly(fup, 2)", covars_formula), collapse = " + ")) %>% 
+                         paste0(c("poly(tstart, 2)", covars_formula), collapse = " + ")) %>% 
     as.formula()  
 }
 ################################################################################
@@ -15,7 +15,7 @@ fit_cens_plr <- function(data_long, formula_cens){
         data = data_long)
 }
 add_p_uncens_plr <- function(data_long, plr_fit){
-  # add linear predictor and calculate probability of remaining uncensored
+  # estimate probability of remaining uncensored
   data_long <-
     data_long %>%
     mutate(p_uncens = predict(plr_fit, type = "response"))
