d73bad2f20871037fa16d222781c08b0f0d38ee3
Linda Nab <linda.nab@thedatalab.org>
2023-11-14 21:03:38 +0000

    calc remdesivir treated

--- analysis/data_import/calc_n_excluded.R
+++ analysis/data_import/calc_n_excluded.R
@@ -25,6 +25,13 @@ calc_n_excluded <- function(data_processed){
     filter(status_all %in% c("covid_death", "noncovid_death") &
              fu_all == 0) %>%
     nrow()
+  n_treated_rem <-
+    data_processed %>%
+    filter(treated_sot_mol_same_day == 0 & treated_pax_on_same_day_as_sot_mol == 0) %>%
+    filter(!(status_all %in% c("covid_hosp", "noncovid_hosp", "covid_death", "noncovid_death") &
+               fu_all == 0)) %>%
+    filter(!is.na(remdesivir_covid_therapeutics)) %>%
+    nrow()
   n_after_exclusion_processing_pax_included <- 
     data_processed %>%
     # Exclude patients treated with both sotrovimab and molnupiravir on the
@@ -62,6 +69,7 @@ calc_n_excluded <- function(data_processed){
                 n_pax_same_day_as_sot_mol,
                 n_hospitalised_pos_test,
                 n_died_pos_test,
+                n_treated_rem,
                 n_after_exclusion_processing_pax_included,
                 n_treated_pax,
                 n_after_exclusion_processing_pax_excluded)
