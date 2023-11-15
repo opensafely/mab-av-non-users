15de94901bc7a30aff4b9e0c8f07ae3e51fe9f02
Linda Nab <linda.nab@thedatalab.org>
2023-11-15 21:57:06 +0000

    add columns for alt trt and pax trt

--- analysis/data_ccw/simplify_data.R
+++ analysis/data_ccw/simplify_data.R
@@ -10,44 +10,42 @@ ccw_simplify_data <- function(data, outcome, contrast, subgrp){
           0),
         # some people have been treated on or after they experience an event,
         # variable 'treatment_prim' is then 'Untreated' (see process_data.R), if so,
-        # set tb_postest_treat (day of fup on which they've been treated to NA)
+        # set tb_postest_treat_ccw (day of fup on which they've been treated to NA)
         tb_postest_treat_ccw = if_else(
           treatment_prim == "Untreated",
           NA_real_,
           tb_postest_treat),
         treatment_ccw = treatment_prim,
         fu_ccw = fu_primary,
-      )
-    if (contrast != "all"){
-      contrast <- contrast %>% stringr::str_to_title()
-      data <- 
-        data %>%
-        filter(treatment_strategy_cat_prim %in% c("Untreated", contrast))
-    }
-  } else if (outcome == "secondary"){
-    data <-
-      data %>%
-      mutate(
-        # simplify primary status
-        status_ccw_simple = if_else(
-          status_secondary %in% c("allcause_hosp_death"),
-          1,
-          0),
         # some people have been treated on or after they experience an event,
-        # variable 'treatment_prim' is then 'Untreated' (see process_data.R), if so,
-        # set tb_postest_treat (day of fup on which they've been treated to NA)
-        tb_postest_treat_ccw = if_else(
-          treatment_sec == "Untreated",
+        # variable 'treatment_paxlovid_prim' is then 'Untreated' (see process_data.R), if so,
+        # set tb_postest_treat_pax_ccw (day of fup on which they've been treated to NA)
+        tb_postest_treat_pax_ccw = if_else(
+          treatment_paxlovid_prim == "Untreated",
           NA_real_,
           tb_postest_treat),
-        treatment_ccw = treatment_sec,
-        fu_ccw = fu_secondary,
+        treatment_paxlovid_ccw = treatment_paxlovid_prim,
       )
     if (contrast != "all"){
       contrast <- contrast %>% stringr::str_to_title()
       data <- 
         data %>%
-        filter(treatment_strategy_cat_sec %in% c("Untreated", contrast))
+        mutate(treatment_ccw = 
+                 if_else(
+                   treatment_strategy_cat_prim %in% c("Untreated", contrast),
+                   treatment_ccw %>% as.character(),
+                   NA_character_) %>% factor(levels = c("Untreated", "Treated")),
+               treatment_alt_ccw =
+                 case_when(
+                   treatment_strategy_cat_prim == "Untreated" ~ "Untreated",
+                   treatment_strategy_cat_prim == contrast ~ "Untreated",
+                   TRUE ~ "Treated") %>% factor(levels = c("Untreated", "Treated")),
+               tb_postest_treat_alt_ccw = 
+                 if_else(
+                   treatment_paxlovid_prim == "Untreated",
+                   NA_real_,
+                   tb_postest_treat)
+               )
     }
   }
   if (subgrp == "haem"){
