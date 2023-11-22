2eb6290a8921964ab149eefe9f87d0e2e3e455dd
Linda Nab <linda.nab@thedatalab.org>
2023-11-22 22:26:13 +0000

    tidy table1 code and add pax group

--- lib/functions/clean_table_names.R
+++ lib/functions/clean_table_names.R
@@ -1,54 +1,48 @@
 #######
 ## Function to clean table names
 clean_table_names = function(input_table) {
-  # Relabel variables for plotting
-  #input_table$Variable[input_table$Variable=="diabetes"] = "Diabetes"
-  
-  # Relabel groups for plotting
-  # demographics
-  input_table$Group[input_table$Group=="ageband"] = "Age"
-  input_table$Group[input_table$Group=="sex"] = "Sex"
-  input_table$Group[input_table$Group=="ethnicity"] = "Ethnicity"
-  input_table$Group[input_table$Group=="imdQ5"] = "IMD"
-  input_table$Group[input_table$Group=="region_nhs"] = "Region"
-  input_table$Group[input_table$Group=="rural_urban"] = "Setting"
-  # clinical characteristics
-  input_table$Group[input_table$Group=="obese"] = "Obesity"
-  input_table$Group[input_table$Group=="smoking_status"] = "Smoking status"
-  input_table$Group[input_table$Group=="diabetes"] = "Diabetes"
-  input_table$Group[input_table$Group=="chronic_cardiac_disease"] = "Chronic Cardiac Disease"
-  input_table$Group[input_table$Group=="copd"] = "COPD"
-  input_table$Group[input_table$Group=="dialysis"] = "Dialysis"
-  input_table$Group[input_table$Group=="serious_mental_illness_nhsd"] = "Severe mental illness"
-  input_table$Group[input_table$Group=="learning_disability_primis"] = "Learning disability"
-  input_table$Group[input_table$Group=="dementia_nhsd"] = "Dementia"
-  input_table$Group[input_table$Group=="autism_nhsd"] = "Autism"
-  input_table$Group[input_table$Group=="care_home_primis"] = "Care home"
-  input_table$Group[input_table$Group=="housebound_opensafely"] = "Housebound"
-  # high risk groups
-  input_table$Group[input_table$Group=="downs_syndrome_nhsd"] = "Down's syndrome"
-  input_table$Group[input_table$Group=="cancer_opensafely_snomed_new"] = "Solid cancer"
-  input_table$Group[input_table$Group=="haematological_disease_nhsd"] = "Haematological diseases"
-  input_table$Group[input_table$Group=="ckd_stage_5_nhsd"] = "Renal disease"
-  input_table$Group[input_table$Group=="liver_disease_nhsd"] = "Liver disease"
-  input_table$Group[input_table$Group=="imid_nhsd"] = "Immune-mediated inflammatory disorders (IMID)"
-  input_table$Group[input_table$Group=="immunosupression_nhsd_new"] = "Immune deficiencies"
-  input_table$Group[input_table$Group=="hiv_aids_nhsd"] = "HIV/AIDs"
-  input_table$Group[input_table$Group=="solid_organ_transplant_nhsd_new"] = "Solid organ transplant"
-  input_table$Group[input_table$Group=="multiple_sclerosis_nhsd"] = "Multiple sclerosis"
-  input_table$Group[input_table$Group=="motor_neurone_disease_nhsd"] = "Motor neurone disease"
-  input_table$Group[input_table$Group=="myasthenia_gravis_nhsd"] = "Myasthenia gravis"
-  input_table$Group[input_table$Group=="huntingtons_disease_nhsd"] = "Huntington’s disease"
-  # vax vars
-  input_table$Group[input_table$Group=="vaccination_status"] = "Vaccination status"
-  input_table$Group[input_table$Group=="tb_postest_vacc_cat"] = "Time-between test since last vaccination"
-  input_table$Group[input_table$Group=="most_recent_vax_cat"] = "Most recent vaccination"
-  input_table$Group[input_table$Group=="pfizer_most_recent_cov_vac"] = "Pfizer"
-  input_table$Group[input_table$Group=="az_most_recent_cov_vac"] = "AstraZeneca"
-  input_table$Group[input_table$Group=="moderna_most_recent_cov_vac"] = "Moderna"
-  #else
-  input_table$Group[input_table$Group=="tb_postest_treat"] = "Time-between test and treatment"
-  input_table$Group[input_table$Group=="treatment_strategy_cat"] = "Treatment group"
-  input_table$Group[input_table$Variable=="N"] = "N"
-  return(input_table)
+  input_table %>%
+    mutate(
+      group = case_when(group == "ageband" ~ "Age",
+                        group == "sex" ~ "Sex",
+                        group == "ethnicity" ~ "Ethnicity",
+                        group == "imdQ5" ~ "IMD",
+                        group == "region_nhs" ~ "Region",
+                        group == "rural_urban" ~ "Setting",
+                        # clinical vars
+                        group == "obese" ~ "Obesity",
+                        group == "smoking_status" ~ "Smoking status",
+                        group == "diabetes" ~ "Diabetes",
+                        group == "hypertension" ~ "Hypertension",
+                        group == "chronic_cardiac_disease" ~ "Chronic Cardiac Disease",
+                        group == "copd" ~ "COPD",
+                        group == "dialysis" ~ "Dialysis",
+                        group == "serious_mental_illness_nhsd" ~ "Severe mental illness",
+                        group == "learning_disability_primis" ~ "Learning disability",
+                        group == "dementia_nhsd" ~ "Dementia",
+                        group == "autism_nhsd" ~ "Autism",
+                        group == "care_home_primis" ~ "Care home",
+                        group == "housebound_opensafely" ~ "Housebound",
+                        # high risk group
+                        group == "downs_syndrome_nhsd" ~ "Down's syndrome",
+                        group == "cancer_opensafely_snomed_new" ~ "Solid cancer",
+                        group == "haematological_disease_nhsd" ~ "Haematological diseases",
+                        group == "ckd_stage_5_nhsd" ~ "CKD stage 5",
+                        group == "liver_disease_nhsd" ~ "Liver disease",
+                        group == "imid_nhsd" ~ "Immune-mediated inflammatory disorders (IMID)",
+                        group == "immunosupression_nhsd_new" ~ "Immune deficiencies",
+                        group == "hiv_aids_nhsd" ~ "HIV/AIDs",
+                        group == "solid_organ_transplant_nhsd_new" ~ "Solid organ transplant",
+                        group == "multiple_sclerosis_nhsd" ~ "Multiple sclerosis",
+                        group == "motor_neurone_disease_nhsd" ~ "Motor neurone disease",
+                        group == "myasthenia_gravis_nhsd" ~ "Myasthenia gravis",
+                        group == "huntingtons_disease_nhsd" ~ "Huntington's disease",
+                        # vax vars
+                        group == "vaccination_status" ~ "Vaccination status",
+                        group == "tb_postest_vacc_cat" ~ "Time-between test since last vaccination",
+                        group == "most_recent_vax_cat" ~ "Most recent vaccination",
+                        group == "pfizer_most_recent_cov_vac" ~ "Pfizer",
+                        group == "az_most_recent_cov_vac" ~ "AstraZeneca",
+                        group == "moderna_most_recent_cov_vac" ~ "Moderna",
+                        TRUE ~ group))
 }
