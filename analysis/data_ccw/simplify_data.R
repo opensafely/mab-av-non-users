be74f5c5c474ebaed849e4c8901b559c01b96ca5
Linda Nab <linda.nab@thedatalab.org>
2023-11-28 22:59:05 +0000

    include noncovid death in outcome io censoring

--- analysis/data_ccw/simplify_data.R
+++ analysis/data_ccw/simplify_data.R
@@ -1,4 +1,27 @@
 ccw_simplify_data <- function(data, outcome, contrast, subgrp){
+  data <-
+    data %>%
+    mutate(
+      # some people have been treated on or after they experience an event,
+      # variable 'treatment_prim' is then 'Untreated' (see process_data.R), if so,
+      # set tb_postest_treat_ccw (day of fup on which they've been treated to NA)
+      tb_postest_treat_ccw = if_else(
+        treatment_prim == "Untreated",
+        NA_real_,
+        tb_postest_treat),
+      treatment_ccw = treatment_prim,
+      fu_ccw = fu_primary,
+      # some people have been treated on or after they experience an event,
+      # variable 'treatment_paxlovid_prim' is then 'Untreated' (see process_data.R), if so,
+      # set tb_postest_treat_pax_ccw (day of fup on which they've been treated to NA)
+      tb_postest_treat_pax_ccw = if_else(
+        treatment_paxlovid_prim == "Untreated",
+        NA_real_,
+        tb_postest_treat),
+      treatment_paxlovid_ccw = treatment_paxlovid_prim,
+      treatment_alt_ccw = "Untreated" %>% factor(levels = c("Untreated", "Treated")),
+      tb_postest_treat_alt_ccw = NA_real_,
+    )
   if (outcome == "primary"){
     data <-
       data %>%
@@ -7,48 +30,39 @@ ccw_simplify_data <- function(data, outcome, contrast, subgrp){
         status_ccw_simple = if_else(
           status_primary %in% c("covid_hosp_death"),
           1,
-          0),
-        # some people have been treated on or after they experience an event,
-        # variable 'treatment_prim' is then 'Untreated' (see process_data.R), if so,
-        # set tb_postest_treat_ccw (day of fup on which they've been treated to NA)
-        tb_postest_treat_ccw = if_else(
-          treatment_prim == "Untreated",
-          NA_real_,
-          tb_postest_treat),
-        treatment_ccw = treatment_prim,
-        fu_ccw = fu_primary,
-        # some people have been treated on or after they experience an event,
-        # variable 'treatment_paxlovid_prim' is then 'Untreated' (see process_data.R), if so,
-        # set tb_postest_treat_pax_ccw (day of fup on which they've been treated to NA)
-        tb_postest_treat_pax_ccw = if_else(
-          treatment_paxlovid_prim == "Untreated",
-          NA_real_,
-          tb_postest_treat),
-        treatment_paxlovid_ccw = treatment_paxlovid_prim,
-        treatment_alt_ccw = "Untreated" %>% factor(levels = c("Untreated", "Treated")),
-        tb_postest_treat_alt_ccw = NA_real_,
+          0)
       )
-    if (contrast != "all"){
-      contrast <- contrast %>% stringr::str_to_title()
-      data <- 
-        data %>%
-        mutate(treatment_ccw = 
-                 if_else(
-                   treatment_strategy_cat_prim %in% c("Untreated", contrast),
-                   treatment_ccw %>% as.character(),
-                   "Untreated") %>% factor(levels = c("Untreated", "Treated")),
-               treatment_alt_ccw =
-                 case_when(
-                   treatment_strategy_cat_prim == "Untreated" ~ "Untreated",
-                   treatment_strategy_cat_prim == contrast ~ "Untreated",
-                   TRUE ~ "Treated") %>% factor(levels = c("Untreated", "Treated")),
-               tb_postest_treat_alt_ccw = 
-                 if_else(
-                   treatment_alt_ccw == "Untreated",
-                   NA_real_,
-                   tb_postest_treat)
-               )
-    }
+  } else if (outcome == "primary_combined"){
+    data <-
+      data %>%
+      mutate(
+        # simplify primary status
+        status_ccw_simple = if_else(
+          status_primary %in% c("covid_hosp_death", "noncovid_death"),
+          1,
+          0)
+      )
+  }
+  if (contrast != "all"){
+    contrast <- contrast %>% stringr::str_to_title()
+    data <- 
+      data %>%
+      mutate(treatment_ccw = 
+               if_else(
+                 treatment_strategy_cat_prim %in% c("Untreated", contrast),
+                 treatment_ccw %>% as.character(),
+                 "Untreated") %>% factor(levels = c("Untreated", "Treated")),
+             treatment_alt_ccw =
+               case_when(
+                 treatment_strategy_cat_prim == "Untreated" ~ "Untreated",
+                 treatment_strategy_cat_prim == contrast ~ "Untreated",
+                 TRUE ~ "Treated") %>% factor(levels = c("Untreated", "Treated")),
+             tb_postest_treat_alt_ccw = 
+               if_else(
+                 treatment_alt_ccw == "Untreated",
+                 NA_real_,
+                 tb_postest_treat)
+      )
   }
   if (subgrp == "haem"){
     data <-
